64a9d60c810bcbe3568ae69d419edba0bc6ce108
Parikshit Jagtap <parikshit.jagtap.20@gmail.com>
2024-10-15 02:20:35 +0530

    PRO-002 | Parikshit | Refcator - Use correct wording in main for taking input

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,33 +11,37 @@ public class Main {
     private static final String[] pieces = new String[]{"Pawn", "King", "Queen"};
     public static void main(String[] args) throws Exception {
 
-        System.out.println("Please enter the Piece Number");
-        System.out.println("Enter 1 for Pawn");
-        System.out.println("Enter 2 for King");
-        System.out.println("Enter 3 for Queen");
-        System.out.println("Enter 0 for Exit");
+        System.out.println("PLEASE SELECT THE NUMBER FOR RESPECTIVE PIECE");
+        System.out.println("ENTER 1 FOR PAWN");
+        System.out.println("ENTER 2 FOR KING");
+        System.out.println("ENTER 3 FOR QUEEN");
+        System.out.println("ENTER 0 FOR EXIT");
         Scanner sc = new Scanner(System.in);
         int pieceInput = sc.nextInt();
         if(pieceInput == 0) {
             System.exit(1);
         }
         else if(pieceInput <= 3){
-            System.out.println("You have selected "+ pieces[pieceInput-1]+", Now please enter its position");
+            ChessBoard cb = new ChessBoard();
+            cb.display();
+            System.out.println();
+
+            System.out.println("YOU HAVE SELECTED "+ pieces[pieceInput-1].toUpperCase()+", " +
+                    "NOW PLEASE ENTER ANY ITS POSITION IN ABOVE FORMAT. EG : G2");
+
             String position = sc.next();
+
             if(position.length() == 2){
-                ChessBoard cb = new ChessBoard();
-                cb.display();
                 IPiece piece = PieceFactory.createPiece(pieces[pieceInput -1]);
                 List<String> outcomes = cb.outComes(piece, position);
+
                 System.out.println("POSSIBLE OUTCOMES ARE : "+outcomes);
             }
             else{
-                System.out.println("Please enter correct position between range [A1..A8...H8]");
+                System.out.println("PLEASE ENTER THE CORRECT POSITION BETWEEN RANGE : [A1..A8...H8]");
             }
 
         }
-
-
     }
 }
 
